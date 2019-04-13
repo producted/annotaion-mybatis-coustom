5e8482e8d9beea6052609fb935d7957742ed67ac
e <17666210109@163.com>
2019-04-13 23:01:51 +0800

    up sql

---
+++ sql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) DEFAULT NULL,
+  `age` int(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
